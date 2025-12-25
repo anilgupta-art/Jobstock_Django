5b36b45d0fef023ccf228edc49d8ab108d216034
rituranjang <110986171+rituranjang@users.noreply.github.com>
2025-12-25 16:18:44 +0530

    job filter

--- App/models.py
+++ App/models.py
@@ -1045,7 +1045,6 @@ class ResumeJobMatch(models.Model):
         related_name='resume_matches',
         help_text="Job posting being matched"
     )
-    
     resume = models.ForeignKey(
         ResumeProcessing, 
         on_delete=models.CASCADE, 
